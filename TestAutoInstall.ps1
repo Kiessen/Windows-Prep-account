8c4f4fd638889cdb76e708b6067bba949766f59b
Kiessen <rkob21@gmail.com>
2024-06-28 10:13:16 +0700

    First commit

---
+++ TestAutoInstall.ps1
@@ -0,0 +1,35 @@
+﻿$ServerPath = "\\192.168.222.222\Share\FirstApp\*"
+#$DestinationFolder = "C:\Users\PejabaTinggi\Downloads\TempInstaller"
+#$DestinationFolder = "C:\Users\Uangme\Downloads\TempInstall"
+$dingtalk= 'https://www.dingtalk.com/win/d/qd=20170420173511985?spm=a3140.10045796.845299.4.327224bfPllieU'
+$arr_Uriapp= 
+#'https://dl.google.com/dl/chrome/install/googlechromestandaloneenterprise64.msi'<#Chrome#>,
+#'https://www.microsip.org/download/MicroSIP-3.21.3.exe'<#MicroSIP#>,
+#'https://download.anydesk.com/AnyDesk.exe'<#AnyDesk#>,
+#'https://dl2.ultraviewer.net/UltraViewer_setup_6.6_en.exe'<#AnyDesk#>,
+#'https://wdl1.pcfg.cache.wpscdn.com/wpsdl/wpsoffice/onlinesetup/distsrc/600.1021/wpsinst/wps_office_inst.exe'<#WPS#>
+#mkdir $DestinationFolder
+#cd $DestinationFolder
+function download_App {
+   foreach ($item in $arr_Uriapp)
+			{
+			Start-BitsTransfer -Source $item -Destination $DestinationFolder
+			}
+Invoke-WebRequest -Uri $dingtalk -OutFile "Dingtalk.exe"
+Invoke-Expression 'ls $DestinationFolder'
+}
+
+function copy_App{
+Copy-Item -Path $ServerPath -Destination $DestinationFolder
+}
+
+function install_App {
+$count = Get-ChildItem -Name
+foreach ($item in $count){
+	Start-Process -FilePath $item -Argument "/VERYSILENT /S /v /qn" -PassThru
+}
+}
+
+#download_App
+#copy_App
+install_App
---
+++ V1.ps1
@@ -0,0 +1,38 @@
+﻿$ipShare = <Server IP>
+$netAccount= <Username Server>
+$netPass= <Pass Server>
+$uname1 = <Username Account1>
+$uname2 = <Username Account2>
+$pass1 = <Pass Account 1>
+$pass2 = <Pass Account 2>
+$DestinationFolder = "$env:USERPROFILE\Downloads\TempInstall"
+$checkTemp = Get-LocalUser |Where {@($uname1,$uname2) -contains $_.Name}
+
+function shareCopy {
+net use \\$ipShare\ /user:$netAccount $netPass
+mkdir $DestinationFolder
+Copy-Item -Path "\\$ipShare\Share\FirstApp\*" -Destination $DestinationFolder
+reg add HKLM\SOFTWARE\Microsoft\PolicyManager\default\Connectivity\AllowBluetooth /v value /t REG_DWORD /d 0 /f
+reg add HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR /v Start /t REG_DWORD /d 4 /f
+}
+
+function disBlusb{
+reg add HKLM\SOFTWARE\Microsoft\PolicyManager\default\Connectivity\AllowBluetooth /v value /t REG_DWORD /d 0 /f
+reg add HKLM\SYSTEM\CurrentControlSet\Services\USBSTOR /v Start /t REG_DWORD /d 4 /f
+}
+
+
+if ($checkTemp.Name -notcontains $uname1){
+New-LocalUser -Name $uname1 -Password ConvertTo-SecureString $pass2 -AsPlainText -Force
+Add-LocalGroupMember -Group "Administrators" -Member $uname1
+}
+elif($checkTemp.Name -notcontains $uname2){
+New-LocalUser -Name $uname2 -Password ConvertTo-SecureString $pass1 -AsPlainText -Force
+Add-LocalGroupMember -Group "Users" -Member $uname2
+}
+else {
+Get-LocalUser |Where { $_.Enabled -Match "True" -and @($uname1,$uname2) -notcontains $_.Name}| disable-localuser
+}
+
+shareCopy
+disBlusb
